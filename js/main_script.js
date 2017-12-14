801e7094e520ef55b72f6d7d1e2a272ae7e2f612
Marc Letourneur <marc@fibronostics.com>
2017-12-14 14:38:14 +0800

    added a dropdown list to select the disease of interest

--- js/main_script.js
+++ js/main_script.js
@@ -1,4 +1,10 @@
-var dda_countries = ['Malaysia', 'Hong Kong', 'Singapore', 'Philippines', 'Thailand', 'India']
+var dda_countries = ['Malaysia', 'Hong Kong', 'Singapore', 'Philippines', 'Thailand']
+
+var xaxes = {
+	'steatosis' : ['x', 'S0', 'S1', 'S2', 'S3'],
+	'activity' : ['x', 'A0', 'A1', 'A2', 'A3', 'A4'],
+	'fibrosis' : ['x', 'F0', 'F1', 'F2', 'F3', 'F4']
+}
 
 var dict_stages_percent_per_country = {
 	"Malaysia": {
@@ -29,18 +35,17 @@ var dict_stages_percent_per_country = {
 		"fibrosis" : [0.531232,0.204011,0.068195,0.082521,0.11404],
 		"steatosis" : [0.325866,0.252546,0.179226,0.242363]
 
-	},
-	"India": {
-		"activity" : [1],
-		"fibrosis" : [1],
-		"steatosis" : []
-
 	}
 }
 
 //Country code
 var width = 900;
 var height = 600;
+
+// Disease by default - fibrosis
+var disease_name = 'fibrosis';
+// Country by default
+var country_name = 'Thailand';
 
 var projection = d3.geoMercator();
 
@@ -64,10 +69,10 @@ d3.json("world_hires.json", function(error, topology) {
 	.append("path")
 	.attr("d", path)
 	.attr("fill", function(d, ind){
-		var country_name = topology.objects.countries.geometries[ind].properties.name
+		country_name = topology.objects.countries.geometries[ind].properties.name
 		d.country = country_name;
 		if(dda_countries.includes(country_name)){
-			return "#d1d1d1";
+			return "#cacaca";
 		}else{
 			return "#e5e5e5";
 		}
@@ -75,9 +80,7 @@ d3.json("world_hires.json", function(error, topology) {
 	.attr("stroke", "white")
 	.attr("stroke-width", 0.15)
 	.on("mouseover", function(d, ind) {
-		console.log(topology.objects.countries.geometries[ind].properties.name)
-		var country_name = topology.objects.countries.geometries[ind].properties.name
-		tooltip.html("<b>" + country_name  + "<br/> ")
+		tooltip.html(d.country  + "<br/> ")
 		.style("left", (d3.event.pageX) + 20 + "px")
 		.style("top", (d3.event.pageY) - 20 + "px");
 		tooltip.transition()
@@ -88,15 +91,14 @@ d3.json("world_hires.json", function(error, topology) {
 		.style("opacity", 0.);
 	})
 	.on("click", function(d, ind){
-		var country_name = d.country
-		draw_barplot(country_name);
+		country_name = d.country
+		draw_barplot(country_name, disease_name);
 		g.selectAll("path")
 		.transition()
 		.duration(500)
 		.attr("fill", function(d, ind){
-			var country_name = d.country
-			if(dda_countries.includes(country_name)){
-				return "#d1d1d1";
+			if(dda_countries.includes(d.country)){
+				return "#cacaca";
 			}else{
 				return "#e5e5e5";
 			}
@@ -110,11 +112,10 @@ d3.json("world_hires.json", function(error, topology) {
 		.attr("fill", function(d, ind){
 			console.log(d3.select(this).attr("fill"));
 			if(d3.select(this).attr("fill")=='orange' || d3.select(this).attr("fill")=='rgb(255, 165, 0)' ){
-				var country_name = d.country
 				console.log(d3.select(this).attr("fill"));
 				console.log(d.country)
 				if(dda_countries.includes(d.country)){
-					return "#d1d1d1";
+					return "#cacaca";
 				}else{
 					return "#e5e5e5";
 				}
@@ -152,7 +153,6 @@ d3.json("world_hires.json", function(error, topology) {
  .translateExtent([[-50, -100], [width + 90, height + 100]])
  .on("zoom",function() {
  	g.attr("transform", "scale(" + 6 + ")translate(" + -690 + "," + -175 + ")" + d3.event.transform);
-	// g.attr("transform", d3.event.transform);
 	g.selectAll("path")  
 	.attr("d", path.projection(projection)); 
 })
@@ -160,27 +160,21 @@ d3.json("world_hires.json", function(error, topology) {
  .on("dblclick.zoom", null);
 
 
- g.transition()
- .duration(4000)
- // .duration(0)
- .attr("transform", "scale(" + 6 + ")translate(" + -690 + "," + -175 + ")");
-
-
- function draw_barplot(country_name){
+ function draw_barplot(country_name, disease_name){
  	g_plot = svg.append('g')
  	.attr('id', 'chart')
- 	.attr('class', 'down')
- 	var ar = dict_stages_percent_per_country
- 	var arr = ar[country_name]
- 	var xaxis = ['x', 'S0', 'S1', 'S2', 'S3']
- 	console.log(arr)
- 	var arrr = Object.assign([], arr.steatosis);
- 	console.log(arrr)
- 	arrr.unshift('steatosis')
+ 	.attr('class', 'down');
+ 	var ar = dict_stages_percent_per_country;
+ 	var arr = ar[country_name];
+ 	var xaxis = xaxes[disease_name];
+ 	console.log(arr);
+ 	var arrr = Object.assign([], arr[disease_name]);
+ 	console.log(arrr);
+ 	arrr.unshift(disease_name);
  	var chart = bb.generate({
  		bindto: "#chart",
  		title: {
- 			text: "Steatosis grades in "+ country_name
+ 			text: disease_name.charAt(0).toUpperCase() + disease_name.substr(1) + " grades in "+ country_name
  		},
  		data: {
  			"x" : "x",
@@ -211,7 +205,7 @@ d3.json("world_hires.json", function(error, topology) {
  		"axis": {
  			"y": {
  				"label": "Proportion of the cohort",
- 				"max": 0.5,
+ 				"max": 1.0,
  				"min": 0,
  				"padding": {
  					"top": 0,
@@ -242,3 +236,44 @@ d3.json("world_hires.json", function(error, topology) {
  	.style('top', 120)
  	.style('left', 0);
  }
+
+// Dropdown button 
+var svg_dropbtn = d3.select('body').append('div')
+.attr('class', 'svg_dropbtn');
+svg_dropbtn.html("<div class='dropdown'> <button onclick='dropdownCallback()'' class='dropbtn'>Disease</button> <div id='dropdown-instance' class='dropdown-disease'> <a href='#' id='steatosis' class='disease_notselected'>Steatosis</a> <a href='#' id='activity' class='disease_notselected'>Activity</a> <a href='#' id='fibrosis' class='disease_notselected'>Fibrosis</a></div></div>");
+svg_dropbtn.style("left", 850 + "px")
+.style("top", 270 + "px");
+
+d3.selectAll('.disease_notselected')
+.on('click', function(){
+	d3.selectAll('disease_selected').attr('class', 'disease_notselected');
+	d3.select(this).attr('class', 'disease_selected');
+	disease_name = d3.select(this).attr('id');
+	draw_barplot(country_name, disease_name);
+})
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdownCallback() {
+	document.getElementById("dropdown-instance").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-disease");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
+// Initiate zoom in
+ g.transition()
+ .duration(4000)
+ .attr("transform", "scale(" + 6 + ")translate(" + -690 + "," + -175 + ")");
